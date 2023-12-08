6c3ecb1eda894e0d4180c9a4ec0816cf1abd9535
suraj <surajjosh7@gmail.com>
2023-12-08 20:36:06 +0530

    new pkg disk-cleanup-cli

---
+++ disk-cleanup-cli/index.js
@@ -0,0 +1,46 @@
+const disk=require('disk-cleanup');
+let option=process.argv[2];
+let path=process.argv[3];
+
+
+if(option=="temp"){
+disk.removeTemporary(path,(err,out)=>{
+    if(err){
+        console.log("an error occured" + err);
+    }
+    else{
+        console.log(out);
+    }
+})
+}
+else if(option=="greater"){
+    disk.filterGreater(path,(err,out)=>{
+        if(err){
+            console.log("an error occured "+ err);
+        }
+        else{
+            console.log(out);
+        }
+    })
+}
+else if(option=="smaller"){
+    disk.filterSmaller(path,(err,out)=>{
+        if(err){
+            console.log("an error occured "+ err);
+        }
+        else{
+            console.log(out);
+        }
+    })
+}
+else if(option=="equal"){
+    disk.filterEqual(path,(err,out)=>{
+        if(err){
+            console.log("an error occured "+ err);
+        }
+        else{
+            console.log(out);
+        }
+    })
+
+}
