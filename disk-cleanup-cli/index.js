fc608a33a6afc364f49daf4ef346cc3c7314a051
suraj <surajjosh7@gmail.com>
2023-12-08 20:41:18 +0530

    updated

--- disk-cleanup-cli/index.js
+++ disk-cleanup-cli/index.js
@@ -6,7 +6,7 @@ let path=process.argv[3];
 if(option=="temp"){
 disk.removeTemporary(path,(err,out)=>{
     if(err){
-        console.log("an error occured" + err);
+        console.log("An error occured" + err);
     }
     else{
         console.log(out);
@@ -16,7 +16,7 @@ disk.removeTemporary(path,(err,out)=>{
 else if(option=="greater"){
     disk.filterGreater(path,(err,out)=>{
         if(err){
-            console.log("an error occured "+ err);
+            console.log("An error occured "+ err);
         }
         else{
             console.log(out);
@@ -26,7 +26,7 @@ else if(option=="greater"){
 else if(option=="smaller"){
     disk.filterSmaller(path,(err,out)=>{
         if(err){
-            console.log("an error occured "+ err);
+            console.log("An error occured "+ err);
         }
         else{
             console.log(out);
@@ -36,7 +36,7 @@ else if(option=="smaller"){
 else if(option=="equal"){
     disk.filterEqual(path,(err,out)=>{
         if(err){
-            console.log("an error occured "+ err);
+            console.log("An error occured "+ err);
         }
         else{
             console.log(out);
