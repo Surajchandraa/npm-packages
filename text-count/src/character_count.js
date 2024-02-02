eb0ee2bc3b28e97ada56468c182b05402c03db94
suraj <surajjosh7@gmail.com>
2024-02-02 09:43:16 +0530

    new features in text-count

--- text-count/src/character_count.js
+++ text-count/src/character_count.js
@@ -1,4 +1,4 @@
-function char_count(str_text){
+function charCount(str_text){
     let count = 0;
 
     for(let i=0;i<str_text.length;i++){
@@ -9,14 +9,14 @@ function char_count(str_text){
 }
 
 
-function word_count(str_text){
+function wordCount(str_text){
     let words = str_text.match(/\b\w+\b/g);
   
     return words ? words.length : 0;
     
 }
 
-function sen_count(text) {
+function senCount(text) {
     let matches = text.match(/[.!?]+(\s|$)/g);
   
    
@@ -25,7 +25,7 @@ function sen_count(text) {
 
 
 
-  function line_count(text) {
+  function lineCount(text) {
    
     let lines = text.split(/\r\n|\r|\n/);
 
@@ -35,8 +35,49 @@ function sen_count(text) {
     return lines.length;
 }
 
-module.exports = { char_count, word_count, sen_count, line_count };
 
+
+function mostCommonwords(text,num=5){
+    let words = text.match(/\b\w+\b/g);
+    let wordcount={};
+   
+
+    words.forEach(element => {
+        wordcount[element]=(wordcount[element] || 0) +1
+    });
+    let sortedWords = Object.keys(wordcount).sort((a, b) => wordcount[b] - wordcount[a]);
+    let top_words=sortedWords.slice(0,num);
+
+    let num_words={};
+    top_words.forEach(el=>{
+        num_words[el]=wordcount[el];
+    })
+
+    return num_words;
+}
+
+
+
+function mostCommonChar(text,num=5){
+    
+    let characters = text.match(/[^ ,.]/g);
+    let charcount={};
+    characters.forEach(element=>{
+        charcount[element]=(charcount[element] || 0)+1;
+    })
+    let sortedchar=Object.keys(charcount).sort((a,b)=>charcount[b]-charcount[a]);
+    let top_char=sortedchar.slice(0,num);
+
+    let num_char={};
+    top_char.forEach(el=>{
+        num_char[el]=charcount[el];
+    })
+
+    return num_char;
+    
+}
+
+module.exports={charCount,wordCount,mostCommonChar,mostCommonwords,lineCount,senCount}
 
 
   
