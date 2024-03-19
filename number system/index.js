3e643b10ec80a59231cc87280be342f8d1057f88
suraj chandra <120117179+Surajchandraa@users.noreply.github.com>
2024-03-19 22:02:02 +0530

    Update index.js

--- number system/index.js
+++ number system/index.js
@@ -1,6 +1,6 @@
 const {checkDivisibility,nComposite,nPrime,isPrime,isComposite}=require('./src/check_prime');
 const {CheckEven,CheckOdd}=require('./src/even_odd');
-const factorial=require('./src/factorial');
+const {Factorial,nFactorial}=require('./src/factorial');
 
 
-module.exports={checkDivisibility,nComposite,nPrime,isPrime,isComposite,CheckEven,CheckOdd,factorial};
+module.exports={checkDivisibility,nComposite,nPrime,isPrime,isComposite,CheckEven,CheckOdd,Factorial, nFactorial};
